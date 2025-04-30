4252ca3cfceb3fadeeb357e5981c5d1cb3a856e0
Hugues Hoppe <hhoppe@gmail.com>
2025-04-30 16:37:14 -0700

    Minor changes so that the py and ipynb files are in-sync when using jupytext

--- mediapy_examples.py
+++ mediapy_examples.py
@@ -1,3 +1,4 @@
+# %%
 # Copyright 2024 The mediapy Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# %%
 # %% [markdown]
 # <!--
 # pylint: disable=line-too-long
